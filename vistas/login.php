7356b4e11eceaa42058a8705ddb83b75db6ed41e
Julio <juliojose3000@gmail.com>
2020-04-19 14:10:26 -0600

    login navvar

---
+++ vistas/login.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html>
+	
+	<head>
+		
+		<meta charset="utf-8">
+		<title>Welcome</title>
+		
+		<link rel="stylesheet" href="menubar_style.css">
+		<link rel="stylesheet" href="myStyles.css">
+		
+	</head>
+
+	<body>
+		
+	  <ul id="menu">
+		<li><a href="index.php">Inicio</a></li>
+		<li><a href="estilos_de_aprendizaje.php">Estilos de aprendizaje</a></li>
+		<li><a href="adivinar_recinto.php">adivinar recinto de origen</a></li>
+		<li><a href="adivinar_sexo.php">Adivinar sexo</a></li>
+		<li><a href="adivinar_estilo_aprendizaje.php">Adivinar estilo de aprendizaje</a></li>
+		<li><a href="tipo_profesor.php">Tipo de profesor</a></li>
+		<li><a href="clasificacion_redes.php">Clasificación de redes</a></li>
+	  </ul>
+		
+		<h1>Bienvenidos</h1>
+
+		
+
+	</body>
+</html>
