0e4c7995a1312a8658da45f44e8808a51ecf2229
Julio <juliojose3000@gmail.com>
2020-04-19 15:07:17 -0600

    login

--- vistas/login.php
+++ vistas/login.php
@@ -7,20 +7,16 @@
 		<title>Welcome</title>
 		
 		<link rel="stylesheet" href="css/menubar_style.css">
+
 		
 	</head>
 
 	<body>
-		
-	  <ul id="menu">
-		<li><a href="index.php">Inicio</a></li>
-		<li><a href="estilos_de_aprendizaje.php">Buscar paquetes</a></li>
-		<li><a href="adivinar_recinto.php">Mapa del sitio</a></li>
-		<li><a href="adivinar_sexo.php">Acerca de nosotros</a></li>
-		<li><a href="adivinar_sexo.php">Iniciar sesión</a></li>
-	  </ul>
-		
+	
 
 
 	</body>
+
+
+
 </html>
