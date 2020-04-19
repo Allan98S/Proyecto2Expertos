9e18452fee55f0977cd02e5ed40c9515b24a3cef
Julio <juliojose3000@gmail.com>
2020-04-19 14:18:28 -0600

    menubar listo

--- vistas/login.php
+++ vistas/login.php
@@ -6,8 +6,7 @@
 		<meta charset="utf-8">
 		<title>Welcome</title>
 		
-		<link rel="stylesheet" href="menubar_style.css">
-		<link rel="stylesheet" href="myStyles.css">
+		<link rel="stylesheet" href="css/menubar_style.css">
 		
 	</head>
 
@@ -15,17 +14,13 @@
 		
 	  <ul id="menu">
 		<li><a href="index.php">Inicio</a></li>
-		<li><a href="estilos_de_aprendizaje.php">Estilos de aprendizaje</a></li>
-		<li><a href="adivinar_recinto.php">adivinar recinto de origen</a></li>
-		<li><a href="adivinar_sexo.php">Adivinar sexo</a></li>
-		<li><a href="adivinar_estilo_aprendizaje.php">Adivinar estilo de aprendizaje</a></li>
-		<li><a href="tipo_profesor.php">Tipo de profesor</a></li>
-		<li><a href="clasificacion_redes.php">Clasificación de redes</a></li>
+		<li><a href="estilos_de_aprendizaje.php">Buscar paquetes</a></li>
+		<li><a href="adivinar_recinto.php">Mapa del sitio</a></li>
+		<li><a href="adivinar_sexo.php">Acerca de nosotros</a></li>
+		<li><a href="adivinar_sexo.php">Iniciar sesión</a></li>
 	  </ul>
 		
-		<h1>Bienvenidos</h1>
 
-		
 
 	</body>
 </html>
