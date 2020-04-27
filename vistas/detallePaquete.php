277a2d8841aad1a04257c9664f6eda959f37ee44
Julio <juliojose3000@gmail.com>
2020-04-27 11:33:45 -0600

    funcionalidad de detalles paquete 60%

--- vistas/detallePaquete.php
+++ vistas/detallePaquete.php
@@ -7,8 +7,7 @@
 		<title>Detalle Paquete</title>
         <script src="js/jquery.min.js"></script>
         <script src="js/bootstrap.min.js"></script>
-        <script src="js/javascript.js"></script>
-		<link rel="stylesheet" href="css/style.css">
+		<link rel="stylesheet" href="css/slideGallery.css">
 		<link rel="stylesheet" href="css/menubar_style.css">
         <link rel="stylesheet" href="css/login.css">
         <link rel="stylesheet" href="css/otrosEstilos.css">
@@ -23,15 +22,18 @@
 
                 var server_url = "http://localhost";
 
-
+                //obtengo los detalles del paquete
                 $.ajax({
                     url:server_url+"/travellersWeb/Proyecto2Expertos/controladores/DetallePaqueteController.php?packageID="+packageID, 
                     dataType : 'json',
                     async : false,
                     success : function(paquete) { 
 
+                        var i = 1;
+                        
+                        //obtengo los destinos turisticos
                         $.ajax({
-                            url:"http://localhost/travellersWeb/Proyecto2Expertos/controladores/DestinoTuristicoController.php?packageID="+packageID, 
+                            url: server_url+"/travellersWeb/Proyecto2Expertos/controladores/DestinoTuristicoController.php?packageID="+packageID, 
                             dataType : 'json',
                             async : false,
                             success : function(destinos) { 
@@ -40,21 +42,24 @@
                             
                                 $.each(destinos, function (key, destino) {    
 
+                                    //obtengo la imagen de cada destino turistico
                                     $.ajax({
-                                        url:"http://localhost/travellersWeb/Proyecto2Expertos/controladores/ImagenesController.php?destinyID="+destino.idtouristdestination, 
+                                        url: server_url+"/travellersWeb/Proyecto2Expertos/controladores/ImagenesController.php?destinyID="+destino.idtouristdestination, 
                                         dataType : 'json',
                                         async : false,
                                         success : function(imagenes) { 
                                             imageURL = imagenes[1].imageURL;
                                         }
                                     });
-                                    
-                                    var htmlCode = "<div class='mySlides'><div class='numbertext'>1 / 6</div>"+
-                                    "<img src="+imageURL+" style='width:450px' alt='Destino'></div>";
 
+                                    var name = (destino.name).split(' ').join('_');
+                                    
+                                    var htmlCode = "<div class='mySlides'><div class='numbertext'>"+ i + "/"+ destinos.length +"</div>"+
+                                    "<img class='images' src="+imageURL+" style='width:450px' alt="+name+"></div>";
+                                    
                                     $("#imageGal").prepend(htmlCode);
 
-        
+                                    i++;
 
                                 });
 
@@ -62,16 +67,18 @@
                             }
                         });
 
-                    $('#titlePackage').text(paquete.name);
-                    $('#detailsPackage').text("Desde: $"+paquete.cost);
+                    $('#titlePackage').text(paquete.name);             
+                    $('#price').text("Desde: $"+paquete.cost);
+                    $("#people").text("Cantidad de personas: "+paquete.numberOfPersons);
+                    $("#startDate").text("Fecha de inicio: "+paquete.startDate);
+                    $("#endDate").text("Fecha de fin: "+paquete.endDate);
 
-                        
+                    showSlides(0);
                     }
 
+                   
 
                 });//paquetes
-
-                plusSlides(0);
 
             });
 
@@ -89,6 +96,31 @@
                     }
                 }
             };
+
+            var indice = 0;
+
+            function showSlides(n){ 
+
+                indice = indice + n;
+
+                var divImages = $(".mySlides");
+
+                var images = $(".images");
+
+
+                if(indice==divImages.length){indice=0}
+                if(indice<0){indice = divImages.length-1}
+
+                for (i = 0; i < divImages.length; i++) {
+                    divImages[i].style.display = "none";
+                }
+
+                divImages[indice].style.display = "block";
+
+                $("#destinyName").text(images[indice].getAttribute("alt").split('_').join(' '));
+
+            }
+
 
         </script>
 
@@ -130,21 +162,24 @@
             <div id="imageGal" class="container">
 
                 <!-- Next and previous buttons -->
-                <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-                <a class="next" onclick="plusSlides(1)">&#10095;</a>
-                <p id="destinyName">Destiny name</p>
+                <a class="prev" onclick="showSlides(-1)">&#10094;</a>
+                <a class="next" onclick="showSlides(1)">&#10095;</a>
+                <div class="divDestinyName">
+                    <p id="destinyName" class="textDestinyNameDatallePaquete"></p>
+                </div>
+                
             </div> 
 
         </div>
 
-        <p id="detailsPackage" class="detallesPackageStyle"></p>
+        <p id="price" class="detallesPackageStyle"></p>
+        <p id="people" class="detallesPackageStyle"></p>
+        <p id="startDate" class="detallesPackageStyle"></p>
+        <p id="endDate" class="detallesPackageStyle"></p>
+
         <button class="boton_personalizado">Recervar paquete</button>
+        
     </div>
-    
-    
-
-
-
 </div>
 
 	</body>
